5014080ebb705c4db54529f869f8e9162222252f
“Mark <mark.temblique@gmail.com>
2025-01-24 11:22:41 +0100

    creazione form - backOffice

---
+++ backOffice.js
@@ -0,0 +1,3 @@
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNjBmYmI3NDcwMTAwMTU4YjJiMmUiLCJpYXQiOjE3Mzc3MTE4NjcsImV4cCI6MTczODkyMTQ2N30.OwPnHMmsZmgoEr48ZzpNHEt6n2qkKnAU1MpqhlcpRNY";
+const URL = "https://striveschool-api.herokuapp.com/api/product/";
